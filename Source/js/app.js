32aa7e37c36e4e7dd82fff056c9d1278e34dd1bf
ankit.passi <apassi@teksystems.com>
2019-02-20 19:52:23 +0530

    New Scene

--- Source/js/app.js
+++ Source/js/app.js
@@ -7,6 +7,10 @@ scene.fog = new THREE.Fog( 0xcce0ff, 500, 10000 );
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 // camera.position.set( 1000, 50, 1500 );
 
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
 scene.add( new THREE.AmbientLight( 0x24454c ) );
 var light = new THREE.DirectionalLight( 0xdfebff, 1 );
 light.position.set( 50, 200, 100 );
@@ -23,16 +27,6 @@ light.shadow.camera.far = 1000;
 scene.add( light );
 
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-var geometry = new THREE.BoxGeometry( 1,1,1 );
-var material = new THREE.MeshNormalMaterial();
-var cube = new THREE.Mesh( geometry, material );
-cube.castShadow = true;
-cube.receiveShadow = true;
-scene.add( cube);
 
 
 // var geometry1 = new THREE.BoxGeometry( 10, 0.125, 1 );
@@ -45,18 +39,42 @@ scene.add( cube);
 
 
 var loader = new THREE.TextureLoader();
-var groundTexture = loader.load( 'https://images.pexels.com/photos/413195/pexels-photo-413195.jpeg' );
-groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
-groundTexture.repeat.set( 25, 25 );
-groundTexture.anisotropy = 16;
-var groundMaterial = new THREE.MeshLambertMaterial( { map: groundTexture } );
-var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 20000, 20000 ), groundMaterial );
-mesh.position.y = - 250;
-mesh.rotation.x = - Math.PI / 2;
-mesh.receiveShadow = true;
-scene.add( mesh );
+
+var geometry = new THREE.BoxGeometry( 1,1,1 );
+var material = new THREE.MeshNormalMaterial();
+var cube = new THREE.Mesh( geometry, material );
+cube.castShadow = true;
+cube.receiveShadow = true;
+scene.add( cube);
+// var GeometryLoader = new THREE.JsonLoader();
+
+// GeometryLoader.load("../models/monkeyData.js");
+// monkey = new Three.Mesh();
+// scene.add(monkey);
+// var groundTexture = loader.load( 'https://images.pexels.com/photos/413195/pexels-photo-413195.jpeg' );
+// groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+// groundTexture.repeat.set( 25, 25 );
+// groundTexture.anisotropy = 16;
+// var groundMaterial = new THREE.MeshLambertMaterial( { map: groundTexture } );
+// var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 20000, 20000 ), groundMaterial );
+// mesh.position.y = - 250;
+// mesh.rotation.x = - Math.PI / 2;
+// mesh.receiveShadow = true;
+// scene.add( mesh );
 
 camera.position.z = 5;
+
+var loader = new THREE.GLTFLoader();
+loader.load(
+   "../models/monkeyModel.gltf",
+   function ( gltf ) 
+   {
+       scene.add( gltf.scene );
+       gltf.scene;
+   },
+);
+scene.add( bus.frame )
+
 
 var animate = function () 
 {
